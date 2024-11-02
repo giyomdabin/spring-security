eba48d49f853c28892887b22aebadb3ae8cf4566
giyomdabin <dabinjang01@gmail.com>
2024-11-03 05:35:41 +0900

    feat: add LoginFilter

---
+++ src/main/java/study/springsecurityexample/jwt/LoginFilter.java
@@ -0,0 +1,45 @@
+package study.springsecurityexample.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.security.sasl.AuthenticationException;
+
+@AllArgsConstructor
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+
+        String email = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        System.out.println(email);
+
+        //UsernamePasswordAuthentication: authenticationManager에게 email, password 를 전달할 때 쓰는 토큰
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(email,password,null);
+
+        return authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+    }
+
+    // 인증 성공시 호출
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
+        System.out.println("sucessful authentication");
+    }
+
+    // 인증 실패시 호출
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
+        System.out.println("failed authentication");
+    }
+}
